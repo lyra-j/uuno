a723c61ffbc7eccf4fb7d718e86a2e939c564ec6
K-jisu <wltn7star@naver.com>
2025-03-31 17:09:50 +0900

    🩹 fix : 환경변수로 변경 #7

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr';
-import ENV from '@/constants/env.constant';
+import { createBrowserClient } from '@supabase/ssr'
 
-export const getBrowserClient = () => {
-  return createBrowserClient(ENV.SUPABASE_URL!, ENV.SUPABASE_ANON_KEY!);
-};
+export function createClient() {
+  // Create a supabase client on the browser with project's credentials
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
