95b85c1cc77d918b8fc578831b24e220c1b89f77
K-jisu <wltn7star@naver.com>
2025-03-31 23:30:06 +0900

    📄 docs : 주석 및 불필요한 import 삭제 #7

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,40 +1,5 @@
-'use client';
-import { useForm, FieldValues } from 'react-hook-form';
-import { login } from '../actions';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import AuthForm from '@/components/auth/AuthForm';
 
-// const loginSchema = z.object({
-//   email: z.string().email({ message: 'invalid email' }),
-//   password: z.string().min(6, { message: '6글자 이상 입력' }),
-// });
-
 export default function LoginPage() {
-  // const { register, handleSubmit, formState } = useForm({
-  //   mode: 'onBlur',
-  //   resolver: zodResolver(loginSchema),
-  // });
-
-  // const handleLogin = async (value: FieldValues) => {
-  //   console.log(formState);
-  //   // await login(value);
-  // };
-  return (
-    <AuthForm type='login' />
-    // <form onSubmit={handleSubmit(handleLogin)}>
-    //   <label htmlFor='email'>Email:</label>
-    //   <input id='email' {...register('email')} />
-    //   {formState.errors.email && (
-    //     <p style={{ color: 'red' }}>{formState.errors.email.message}</p>
-    //   )}
-    //   <label htmlFor='password'>Password:</label>
-    //   <input id='password' {...register('password')} />
-    //   {formState.errors.password && (
-    //     <p style={{ color: 'red' }}>{formState.errors.password.message}</p>
-    //   )}
-
-    //   <button type='submit'>Log in</button>
-    // </form>
-  );
+  return <AuthForm type='login' />;
 }
