93c89e2fec54d97e7059eecf6936f58619f291d6
K-jisu <wltn7star@naver.com>
2025-04-01 19:25:25 +0900

    🎉 add : email 중복 검사 로직 추가 #9

---
+++ src/utils/duplicate-validation.ts
@@ -0,0 +1,21 @@
+import { TABLES } from '@/constants/tables';
+import { createClient } from './supabase/server';
+import { ERROR_MESSAGES } from '@/constants/messages';
+
+export const duplicateValudation = async (email : string) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from(TABLES.USERS)
+    .select('*')
+    .eq('email', email)
+    .single();
+
+  /** 중복 아님 */
+  if (error) {
+    return false;
+  }
+
+  /** 중복 */
+  console.error(ERROR_MESSAGES.EMAIL_DUPLICATED_ERROR, error);
+  return !!data;
+};
