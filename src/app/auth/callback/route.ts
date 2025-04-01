efb6d31a53827936b88e8b1f9ddaa5540e193463
K-jisu <wltn7star@naver.com>
2025-04-01 19:26:31 +0900

    ✨ update : 타입 가드 추가 #9

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,12 +3,17 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 import { ERROR_MESSAGES } from '@/constants/messages';
 import { TABLES } from '@/constants/tables';
+import { duplicateValudation } from '@/utils/duplicate-validation';
 
 export const GET = async (request: Request) => {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/';
+
+  if (!code) {
+    /** 에러 페이지는 나중에 같이 고민해봐야 할 듯 */
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  }
 
   if (code) {
     const supabase = await createClient();
@@ -17,25 +22,35 @@ export const GET = async (request: Request) => {
       const userId = data.user?.id;
       const nick_name = data.user.user_metadata.full_name;
       const email = data.user.email;
+
       /** forwardedHost : localhost:3000 */
       const forwardedHost = request.headers.get('x-forwarded-host');
       /** 개발 환경인지 */
       const isLocalEnv = process.env.NODE_ENV === 'development';
 
-      // public users에 데이터 삽입
-      const { error: insertError } = await supabase.from(TABLES.USERS).insert([
-        {
-          id: userId,
-          nick_name,
-          email,
-        },
-      ]);
-      if (insertError) {
-        console.error(ERROR_MESSAGES.USERS_TABLE_INSERT_ERROR, insertError);
+      if (!email) {
+        throw new Error(ERROR_MESSAGES.NONE_EMAIL_ERROR);
+      }
+
+      /** 중복이 아닐시 */
+      if (!(await duplicateValudation(email))) {
+        // public users에 데이터 삽입
+        const { error: insertError } = await supabase
+          .from(TABLES.USERS)
+          .insert([
+            {
+              id: userId,
+              nick_name,
+              email,
+            },
+          ]);
+        if (insertError) {
+          console.error(ERROR_MESSAGES.USERS_TABLE_INSERT_ERROR, insertError);
+        }
       }
 
       if (isLocalEnv) {
-        /** 개발 환경 일 시 localhost:3000 으로 이동 */
+        /** 개발 환경일 시 localhost:3000 으로 이동 */
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
         /** 개발환경 아닐 시 "/" 로 이동 */
@@ -45,7 +60,4 @@ export const GET = async (request: Request) => {
       }
     }
   }
-
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 };
