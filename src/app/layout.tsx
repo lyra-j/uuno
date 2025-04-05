eb46c541de716f6d80231e4e4e3dd8808a9fa74b
K-jisu <wltn7star@naver.com>
2025-04-05 16:57:15 +0900

    📄 docs : 파일 명 수정 #9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import Providers from '@/providers/TQProvider';
+import Providers from '@/providers/tq-provider';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
