3fb60db026731c24fd5e9371ff164f77695361ac
woozizi <c980805@naver.com>
2025-04-07 14:54:49 +0900

    🎉 add : 1.5section 추가  #10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Section2 from '@/components/main/section-two';
 import Section3 from '@/components/main/section-three';
 import Section5 from '@/components/main/section-five';
 import Section4 from '@/components/main/section-four';
+import SectionOnePointFive from '@/components/main/section-one-point-five';
 
 export default function Homepage() {
   return (
     <div className='flex flex-col'>
       <Section1 />
+      <SectionOnePointFive />
       <Section2 />
       <Section3 />
       <Section4 />
---
+++ src/components/main/section-one-point-five.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SectionOnePointFive = () => {
+  return (
+    <div className='flex h-[395px] flex-col items-center justify-center bg-[linear-gradient(0deg,_#F7F9FC_0%,_#FFF_100%)]'>
+      <h1 className='mb-[30px] text-4xl font-bold'>
+        <span className='text-blue-500'>Uuno 디지털 명함,</span> 스마트하게 나를
+        PR하는 법
+      </h1>
+      <p className='text-3xl leading-10'>
+        누구나 쉽게 만들고, 간편하게 공유하는
+        <br /> 스마트 명함으로 당신을 소개해보세요.
+      </p>
+    </div>
+  );
+};
+
+export default SectionOnePointFive;
