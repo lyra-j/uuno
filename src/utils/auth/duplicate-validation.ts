31e3061dbbbceb6dff00c459bb84da36777245b8
K-jisu <wltn7star@naver.com>
2025-04-07 22:28:07 +0900

    🚚 move : 파일, 폴더 위치 변경 #25

---
+++ src/utils/auth/duplicate-validation.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { TABLES } from '@/constants/tables';
+import { ERROR_MESSAGES } from '@/constants/messages';
+import { User } from '@/types/supabase.type';
+import { getSingleData } from '@/services/common.server.dto';
+
+export const duplicateEmailValidation = async (email: string) => {
+  const { data, error } = await getSingleData<User>({
+    table: TABLES.USERS,
+    field: 'email',
+    value: email,
+  });
+
+  /** 중복 아님 */
+  if (error) {
+    return false;
+  }
+
+  /** 중복 */
+  console.error(ERROR_MESSAGES.EMAIL_DUPLICATED_ERROR, error);
+  return !!data;
+};
+
+export const duplicateNickNameValidation = async (nick_name: string) => {
+  const { data, error } = await getSingleData<User>({
+    table: TABLES.USERS,
+    field: 'nick_name',
+    value: nick_name,
+  });
+
+  /** 중복 아님 */
+  if (error) {
+    return false;
+  }
+
+  /** 중복 */
+  console.error(ERROR_MESSAGES.NICKNAME_DUPLICATED_ERROR, error);
+  return !!data;
+};
---
+++ src/utils/auth/validate-schema.ts
@@ -0,0 +1,53 @@
+import { VALIDATE } from '@/constants/messages';
+import { z } from 'zod';
+
+const commonSchema = {
+  email: z.string().email({ message: VALIDATE.INVALID_EMAIL }),
+  password: z
+    .string()
+    .min(VALIDATE.MIN_PASSWORD_LENGTH, {
+      message: VALIDATE.MIN_PASSWORD_MESSAGE,
+    })
+    .max(VALIDATE.MAX_PASSWORD_LENGTH, {
+      message: VALIDATE.MAX_PASSWORD_MESSAGE,
+    })
+    .regex(VALIDATE.STRING_PASSWORD_REGEX, {
+      message: VALIDATE.STRING_PASSWORD_MESSAGE,
+    })
+    .regex(VALIDATE.NUMBER_PASSWORD_REGEX, {
+      message: VALIDATE.STRING_PASSWORD_MESSAGE,
+    }),
+};
+
+export const loginSchema = z.object(commonSchema);
+
+export const signUpSchema = z
+  .object({
+    ...commonSchema,
+    confirmPassword: z
+      .string()
+      .min(VALIDATE.MIN_PASSWORD_LENGTH, {
+        message: VALIDATE.MIN_PASSWORD_MESSAGE,
+      })
+      .max(VALIDATE.MAX_PASSWORD_LENGTH, {
+        message: VALIDATE.MAX_PASSWORD_MESSAGE,
+      })
+      .regex(VALIDATE.STRING_PASSWORD_REGEX, {
+        message: VALIDATE.STRING_PASSWORD_MESSAGE,
+      })
+      .regex(VALIDATE.NUMBER_PASSWORD_REGEX, {
+        message: VALIDATE.STRING_PASSWORD_MESSAGE,
+      }),
+    nick_name: z
+      .string()
+      .min(VALIDATE.MIN_NICKNAME_LENGTH, {
+        message: VALIDATE.MIN_NICKNAME_MESSAGE,
+      })
+      .max(VALIDATE.MAX_NICKNAME_LENGTH, {
+        message: VALIDATE.MAX_NICKNAME_MESSAGE,
+      }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: VALIDATE.INVALID_PASSWORD,
+    path: ['confirmPassword'],
+  });
