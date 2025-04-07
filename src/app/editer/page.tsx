d63881dd1504cb3878df1fc792ecc2651cf4a941
woozizi <c980805@naver.com>
2025-04-08 01:33:35 +0900

    🎉 add : konva 초기 세팅 #30

---
+++ src/app/editer/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const editPage = () => {
+  return <div>editPage</div>;
+};
+
+export default editPage;
---
+++ src/components/editer/CanvasEditor.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CanvasEditor = () => {
+  return <div>CanvasEditor</div>;
+};
+
+export default CanvasEditor;
