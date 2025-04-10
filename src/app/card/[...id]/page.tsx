c2ebb103d8fbb82c254040c83589755268034e80
노수민 <nosumin29@gmail.com>
2025-04-10 21:08:56 +0900

    📄 docs : 페이지 레이아웃에 주석 추가 #38

--- src/app/card/[...id]/page.tsx
+++ src/app/card/[...id]/page.tsx
@@ -7,6 +7,7 @@ const page = () => {
   return (
     <>
       <div className='flex items-center border-b border-solid border-b-zinc-100 p-6'>
+        {/* 페이지 타이틀 */}
         <div className='mx-auto flex w-full max-w-5xl items-center justify-start'>
           <button className='mr-2'>
             <svg
@@ -29,15 +30,19 @@ const page = () => {
       </div>
       <div className='mx-auto max-w-5xl'>
         <div className='grid grid-cols-3'>
+          {/* 왼쪽 컬럼 */}
           <div className='col-span-1 flex flex-col border-r border-[#F4F4F5] p-[14px] text-[18px] shadow-[0px_3px_18px_0px_rgba(0,0,0,0.04)]'>
             <select name='' id='' className='py-2'>
               <option value='test1'>test1</option>
               <option value='test2'>test2</option>
               <option value='test3'>test3</option>
             </select>
+            {/* 명함 플립 */}
             <FlipCard />
           </div>
+          {/* 오른쪽 컬럼 - 통계 정보 */}
           <div className='col-span-2 bg-[#F5F6FA]'>
+            {/* 통계 헤더 */}
             <div className='flex items-center justify-between bg-white p-4 shadow-[0px_4px_20px_0px_rgba(0,0,0,0.04)]'>
               <h2 className='text-lg font-bold'>내 명함 통계</h2>
               <div className='flex gap-2'>
@@ -50,8 +55,13 @@ const page = () => {
               </div>
             </div>
             <div className='p-5'>
+              {/* 통계 카드 그리드 */}
               <StatCardGrid />
+
+              {/* 주간 통계 차트 */}
               <WeeklyChart />
+
+              {/* 경로 분석 차트 그리드 */}
               <PathAnalysisGrid />
             </div>
           </div>
