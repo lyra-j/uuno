d9a1007fd4add2c90025cc8a622f6c0c04f9b3bf
lyra <mk.lyra.j@gmail.com>
2025-04-11 22:17:04 +0900

    🌐 chore : 컬러칩 추가 #61

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        // 기본 색상
+        black: '#1A1A1A',
+        white: '#FFFFFF',
+        bg: '#F5F5F7',
+        error: '#FF1C1C',
+
+        // 회색 계열
         gray: {
           '5': '#F4F4F5',
           '10': '#E1E2E4',
@@ -27,6 +34,28 @@ const config: Config = {
           '90': '#46474C',
           '100': '#37383C',
         },
+
+        // 조회수/막대 차트 색상
+        chart1: {
+          'increase': '#009DD6', // 명함 상세 - 조회 수/증가
+          'same': '#00B69B', // 명함 상세 - 조회 수/동일
+          'decrease': '#F93C65', // 명함 상세 - 조회 수/하락
+          'tag': '#FFD548', // 명함 상세 - 막대차트/Tag
+          'qr': '#FFA69B', // 명함 상세 - 막대차트/QR
+          'link': '#657AFF', // 명함 상세 - 막대차트/Link
+          'direct': '#00D7C0', // 명함 상세 - 막대차트/Direct
+        },
+
+        // 도넛 차트 색상
+        chart2: {
+          'insta': '#6792CF', // 원형 그래프/인스타
+          'github': '#6FD7AE', // 원형 그래프/깃 허브
+          'linkedin': '#8690EE', // 원형 그래프/링크드인
+          'image': '#FF7800', // 원형 그래프/이미지 저장
+          'kakao': '#FFB30A', // 원형 그래프/카카오톡
+          'notion': '#64B0F9', // 원형 그래프/노션
+          'vcard': '#FF143F', // 원형 그래프/연락처 저장
+        },
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
@@ -35,6 +64,8 @@ const config: Config = {
           DEFAULT: 'hsl(var(--popover))',
           foreground: 'hsl(var(--popover-foreground))',
         },
+
+        // 기본 파란색 계열 (프라이머리)
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
