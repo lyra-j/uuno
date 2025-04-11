29203699137726fb262c6f6dbdc20483994e1740
노수민 <nosumin29@gmail.com>
2025-04-11 15:55:59 +0900

    ✨ update : 통계 카드 그리드에 월간 저장 수 및 변화량 표시 기능 추가 #54

--- src/components/card-detail/stat-card-grid.tsx
+++ src/components/card-detail/stat-card-grid.tsx
@@ -1,11 +1,32 @@
-import StatCard from './stat-card';
+'use client';
 
-const StatCardGrid = () => (
-  <div className='mb-6 grid grid-cols-1 gap-4 md:grid-cols-3'>
-    <StatCard title='월간 조회 수' />
-    <StatCard title='월간 저장 수' />
-    <StatCard title='월 평균 체류 시간' />
-  </div>
-);
+import { useParams } from 'next/navigation';
+import StatCard from './stat-card';
+import useMonthSaveCnt from '@/hooks/queries/use-month-save-cnt';
+
+const StatCardGrid = () => {
+  const params = useParams();
+
+  const {
+    data: monthSaveData,
+    isPending: monthSaveIsPending,
+    error: monthSaveError,
+  } = useMonthSaveCnt(params?.id[0]);
+  if (monthSaveIsPending) return <div>Loading...</div>;
+  if (monthSaveError) return <div>{monthSaveError.message}</div>;
+
+  return (
+    <div className='mb-6 grid grid-cols-1 gap-4 md:grid-cols-3'>
+      <StatCard title='월간 조회 수' />
+      <StatCard
+        title='월간 저장 수'
+        value={monthSaveData?.currentMonthCount}
+        statusData={monthSaveData?.difference}
+        unit='회'
+      />
+      <StatCard title='월 평균 체류 시간' />
+    </div>
+  );
+};
 
 export default StatCardGrid;
