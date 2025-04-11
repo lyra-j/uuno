471ce0d4b89abaa8982591aee7c2fa992e575dd8
노수민 <nosumin29@gmail.com>
2025-04-11 17:01:30 +0900

    ✨ update : 카드 뷰 API 및 커스텀 훅에서 상수 사용으로 코드 가독성 향상 #54

--- src/hooks/queries/use-month-save-cnt.ts
+++ src/hooks/queries/use-month-save-cnt.ts
@@ -1,9 +1,10 @@
 import { getMonthSaveCount } from '@/apis/card-view.api';
+import { QUERY_KEY } from '@/constants/query-key';
 import { useQuery } from '@tanstack/react-query';
 
 const useMonthSaveCnt = (cardId: string) => {
   return useQuery({
-    queryKey: ['month-save', cardId],
+    queryKey: [QUERY_KEY.MONTH_SAVE, cardId],
     queryFn: async () => getMonthSaveCount(cardId),
     staleTime: 1000 * 60 * 5,
   });
