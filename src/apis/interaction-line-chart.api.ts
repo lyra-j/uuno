608c4496bc5e55f5ea9cc442e7c4f16e913f4044
노수민 <nosumin29@gmail.com>
2025-04-11 23:26:43 +0900

    🩹 refactor : getInteractionLineChartData 함수에서 중복 코드 제거 및 공통 함수로 추출 #54

--- src/apis/interaction-line-chart.api.ts
+++ src/apis/interaction-line-chart.api.ts
@@ -1,4 +1,8 @@
-import { DB_COLUMNS, TABLES } from '@/constants/tables.constant';
+import {
+  CARD_VIEW_TYPES,
+  DB_COLUMNS,
+  TABLES,
+} from '@/constants/tables.constant';
 import { getCurrentWeekRange } from '@/utils/card-detail/week-range.util';
 import { createClient } from '@/utils/supabase/client';
 
@@ -12,38 +16,34 @@ export const getInteractionLineChartData = async ({
   const { start, end, weekDates } = getCurrentWeekRange();
   const supabase = await createClient();
 
+  // 공통 함수로 추출
+  const getCountByDate = async (date: string, type?: 'click' | 'save') => {
+    let query = supabase
+      .from(TABLES.CARD_VIEWS)
+      .select('*', { count: 'exact', head: true })
+      .gte(DB_COLUMNS.CARD_VIEWS.STARTED_AT, (date + 'T00:00:00').toString())
+      .lte(DB_COLUMNS.CARD_VIEWS.END_AT, (date + 'T23:59:59').toString())
+      .order(DB_COLUMNS.CARD_VIEWS.STARTED_AT, { ascending: true })
+      .eq(DB_COLUMNS.CARD_VIEWS.CARD_ID, card_id);
+
+    if (type) {
+      query = query.eq('type', type);
+    }
+
+    const { count, error } = await query;
+    if (error) {
+      console.error(error);
+      return;
+    }
+    return count;
+  };
+
   const weekViewCnt = await Promise.all(
-    weekDates.map(async (date) => {
-      const { count, error } = await supabase
-        .from(TABLES.CARD_VIEWS)
-        .select('*', { count: 'exact', head: true })
-        .gte(DB_COLUMNS.CARD_VIEWS.STARTED_AT, (date + 'T00:00:00').toString())
-        .lte(DB_COLUMNS.CARD_VIEWS.END_AT, (date + 'T23:59:59').toString())
-        .order(DB_COLUMNS.CARD_VIEWS.STARTED_AT, { ascending: true })
-        .eq(DB_COLUMNS.CARD_VIEWS.CARD_ID, card_id);
-      if (error) {
-        console.error(error);
-        return 0;
-      }
-      return count;
-    })
+    weekDates.map((date) => getCountByDate(date))
   );
+
   const weekSaveCnt = await Promise.all(
-    weekDates.map(async (date) => {
-      const { count, error } = await supabase
-        .from(TABLES.CARD_VIEWS)
-        .select('*', { count: 'exact', head: true })
-        .gte(DB_COLUMNS.CARD_VIEWS.STARTED_AT, (date + 'T00:00:00').toString())
-        .lte(DB_COLUMNS.CARD_VIEWS.END_AT, (date + 'T23:59:59').toString())
-        .order(DB_COLUMNS.CARD_VIEWS.STARTED_AT, { ascending: true })
-        .eq(DB_COLUMNS.CARD_VIEWS.CARD_ID, card_id)
-        .eq('type', 'save');
-      if (error) {
-        console.error(error);
-        return;
-      }
-      return count;
-    })
+    weekDates.map((date) => getCountByDate(date, CARD_VIEW_TYPES.SAVE))
   );
 
   return {
