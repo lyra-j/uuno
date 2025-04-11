40c4d96f385bd4bcec3215c65b8de91f9038e4e0
노수민 <nosumin29@gmail.com>
2025-04-11 23:17:42 +0900

    🩹 fix : useWeekChart 훅에서 queryKey에 card_id 추가하여 데이터 로드 개선 #54

--- src/hooks/queries/use-week-chart.ts
+++ src/hooks/queries/use-week-chart.ts
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 
 const useWeekChart = (card_id: string) => {
   return useQuery({
-    queryKey: [QUERY_KEY.WEEK_CHART],
+    queryKey: [QUERY_KEY.WEEK_CHART, card_id],
     queryFn: async () => getInteractionLineChartData({ card_id }),
   });
 };
