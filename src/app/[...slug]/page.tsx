40dd8c22a0d40897fafae3521fcfb8e00c7a3c2f
노수민 <nosumin29@gmail.com>
2025-04-13 04:12:17 +0900

    🎉 add : 새로운 페이지 컴포넌트 추가 및 FlipCard 사용

---
+++ src/app/[...slug]/page.tsx
@@ -0,0 +1,12 @@
+import FlipCard from '@/components/card/flip-card';
+
+const page = () => {
+  return (
+    <div>
+      <h2>노비님의 포트폴리오 명함</h2>
+      <FlipCard />
+    </div>
+  );
+};
+
+export default page;
