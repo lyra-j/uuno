aa445e1ccdac76a7f4ab566e95b933182ed502ad
노수민 <nosumin29@gmail.com>
2025-04-13 05:02:09 +0900

    💄 style : FlipCard 컴포넌트에 attached prop 추가 및 LeftNavSection에서 사용 #71

--- src/components/card-detail/left-nav-section.tsx
+++ src/components/card-detail/left-nav-section.tsx
@@ -4,7 +4,7 @@ const LeftNavSection = () => {
   return (
     <div className='relative flex flex-1 flex-col items-center justify-between'>
       <div className='flex w-full flex-col items-center justify-center'>
-        <FlipCard />
+        <FlipCard attached={true} />
         <button className='mb-2 flex w-full justify-center rounded-full bg-primary-40 px-3 py-[10px] text-label2-regular text-white'>
           편집하기
         </button>
--- src/components/card/flip-card.tsx
+++ src/components/card/flip-card.tsx
@@ -3,17 +3,21 @@ import clsx from 'clsx';
 import { useState } from 'react';
 import FlipArrow from '../icons/flip-arrow';
 
-const FlipCard = () => {
+interface FlipCardParam {
+  attached?: boolean;
+}
+
+const FlipCard = ({ attached }: FlipCardParam) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const CARD_DEFAULT_STYLE =
     'bg-slate-700 absolute flex justify-center items-center backface-hidden w-full shadow-[37px_108px_32px_0px_rgba(0,0,0,0.00),24px_69px_29px_0px_rgba(0,0,0,0.01),13px_39px_25px_0px_rgba(0,0,0,0.05),6px_17px_18px_0px_rgba(0,0,0,0.09),1px_4px_10px_0px_rgba(0,0,0,0.10)] aspect-[9/5]';
   const CARD_DEFAULT_WRAPPER_STYLE =
     'relative transition-transform duration-1000 cursor-pointer transform-style-preserve-3d';
-  const CARD_DEFAULT_BUTTON_STYLE =
-    'absolute bottom-0 z-10 h-[34px] w-[34px] cursor-pointer';
+  const CARD_DEFAULT_BUTTON_STYLE = 'z-10 h-[34px] w-[34px] cursor-pointer';
+  const CARD_DEFAULT_BUTTON_STYLE_ATTACHED = 'absolute bottom-0';
 
   return (
-    <div className='relative mb-4 flex w-full items-center justify-center'>
+    <div className='relative mb-4 flex w-full flex-col items-center justify-center'>
       <div className='m-5 aspect-[9/5] w-full perspective-1000'>
         <div
           className={clsx(
@@ -27,7 +31,10 @@ const FlipCard = () => {
       </div>
       <div
         onClick={() => setIsFlipped((pre: boolean) => !pre)}
-        className={CARD_DEFAULT_BUTTON_STYLE}
+        className={clsx(
+          CARD_DEFAULT_BUTTON_STYLE,
+          attached && CARD_DEFAULT_BUTTON_STYLE_ATTACHED
+        )}
       >
         <FlipArrow />
       </div>
