3e3e799a20e214d3663a5f3eff6a76eadece98b0
woozizi <c980805@naver.com>
2025-04-14 03:46:02 +0900

    ♻️ refactor : h1 h2로 변경 #82

--- src/components/editor/elements/text/text-sidebar.tsx
+++ src/components/editor/elements/text/text-sidebar.tsx
@@ -84,7 +84,7 @@ const TextSidebar = () => {
 
   return (
     <div className='w-full space-y-4 p-[18px]'>
-      <h1>텍스트 추가</h1>
+      <h2>텍스트 추가</h2>
       <button
         className='w-full bg-gray-50 px-4 py-2 text-white'
         onClick={() => {
