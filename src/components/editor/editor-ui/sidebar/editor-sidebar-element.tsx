455d208f8d148a29ab4ce24fe69026f56f24bf14
K-jisu <wltn7star@naver.com>
2025-04-13 21:09:17 +0900

    ✨update : 조건부 렌더링 추가 #80

--- src/components/editor/editor-ui/sidebar/editor-sidebar-element.tsx
+++ src/components/editor/editor-ui/sidebar/editor-sidebar-element.tsx
@@ -1,22 +1,40 @@
-import { CATEGORY } from '@/constants/editor.constant';
-import ElementsBackgrounds from '../../elements/backgrounds/elements-backgrounds';
-import ElementsDiagrams from '../../elements/diagrams/elements-diagrams';
-import ElementsPictures from '../../elements/pictures/elements-pictures';
-import ElementsSocials from '../../elements/qr-social/elements-social';
 import ElementsTemplates from '../../elements/templates/elements-templates';
 import ElementsUploads from '../../elements/uploads/elements-uploads';
 import TextSidebar from '../../elements/text/text-sidebar';
+import { useEditorStore } from '@/store/editor.store';
+import { CATEGORY } from '@/constants/editor.constant';
+import ElementsPictures from '../../elements/pictures/elements-pictures';
+import ElementsDiagrams from '../../elements/diagrams/elements-diagrams';
+import ElementsBackgrounds from '../../elements/backgrounds/elements-backgrounds';
+import ElementsSocials from '../../elements/qr-social/elements-social';
 
 const EditorSidebarElement = ({ category }: { category: string }) => {
+  const selectedElementType = useEditorStore(
+    (state) => state.selectedElementType
+  );
+  const convertEngToKor = (type: string | null) => {
+    switch (type) {
+      case 'text':
+        return CATEGORY.TEXT;
+      case 'image':
+        return CATEGORY.PICTURE;
+      case 'upload':
+        return CATEGORY.UPLOAD;
+    }
+  };
+  const convertType = convertEngToKor(selectedElementType);
+
+  const finalCategory = selectedElementType ? convertType : category;
+
   return (
     <div className='flex w-60 flex-col'>
-      {category === CATEGORY.TEMPLATE && <ElementsTemplates />}
-      {category === CATEGORY.PICTURE && <ElementsPictures />}
-      {category === CATEGORY.UPLOAD && <ElementsUploads />}
-      {category === CATEGORY.ELEMENT && <ElementsDiagrams />}
-      {category === CATEGORY.TEXT && <TextSidebar />}
-      {category === CATEGORY.BACKGROUND && <ElementsBackgrounds />}
-      {category === CATEGORY.SOCIAL && <ElementsSocials />}
+      {finalCategory === CATEGORY.TEMPLATE && <ElementsTemplates />}
+      {finalCategory === CATEGORY.PICTURE && <ElementsPictures />}
+      {finalCategory === CATEGORY.UPLOAD && <ElementsUploads />}
+      {finalCategory === CATEGORY.ELEMENT && <ElementsDiagrams />}
+      {finalCategory === CATEGORY.TEXT && <TextSidebar />}
+      {finalCategory === CATEGORY.BACKGROUND && <ElementsBackgrounds />}
+      {finalCategory === CATEGORY.SOCIAL && <ElementsSocials />}
     </div>
   );
 };
