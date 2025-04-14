19a1ae49445b053cb2bc3f08541dce75cb08bf56
K-jisu <wltn7star@naver.com>
2025-04-14 19:58:17 +0900

    ✨update : zoom store 추가#45

--- src/store/editor.sidebar.store.ts
+++ src/store/editor.sidebar.store.ts
@@ -11,6 +11,6 @@ export const sideBarStore = create<SideBar>()((set) => ({
   sidebarStatus: false,
   setSideBarStatus: (status) => set({ sidebarStatus: status }),
 
-  zoom: 1,
+  zoom: 1.5,
   setZoom: (status) => set({ zoom: status }),
 }));
