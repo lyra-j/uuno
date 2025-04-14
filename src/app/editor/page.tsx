4c585f422a362560a09f15a67f6223eda40320be
K-jisu <wltn7star@naver.com>
2025-04-14 18:01:58 +0900

    ✨update : undo/redo 키바인딩 #45

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -4,10 +4,39 @@ import EditorCanvas from '@/components/editor/editor-canvas';
 import EditorBottomTab from '@/components/editor/editor-ui/bottomTab/editor-bottom-tab';
 import EditorSideBar from '@/components/editor/editor-ui/sidebar/editor-sidebar';
 import EditorTopbar from '@/components/editor/editor-ui/topbar/editor-topbar';
-import { useRef } from 'react';
+import { useEditorStore } from '@/store/editor.store';
+import { useEffect, useRef } from 'react';
 
 const EditPage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const redo = useEditorStore((state) => state.redo);
+  const undo = useEditorStore((state) => state.undo);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+      const ctrlOrCmd = isMac ? e.metaKey : e.ctrlKey;
+
+      if (ctrlOrCmd && e.key.toLowerCase() === 'z') {
+        e.preventDefault();
+        if (e.shiftKey) {
+          // Ctrl+Shift+Z or Cmd+Shift+Z
+          redo();
+        } else {
+          // Ctrl+Z or Cmd+Z
+          undo();
+        }
+      }
+      // Ctrl + y
+      if (ctrlOrCmd && e.key.toLocaleLowerCase() === 'y') {
+        e.preventDefault();
+        redo();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [undo, redo]);
 
   return (
     <div className='flex h-[calc(100vh-64px)] flex-row overflow-hidden'>
