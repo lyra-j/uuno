bfd522d8a252603f2cdfc5c356a40d9b7a208399
K-jisu <wltn7star@naver.com>
2025-04-15 21:00:02 +0900

    ♻️ refactor : 앞면 뒷면 조건에 따라 undo/redo 변경 #104

--- src/components/editor/editor-ui/topbar/editor-topbar.tsx
+++ src/components/editor/editor-ui/topbar/editor-topbar.tsx
@@ -5,15 +5,24 @@ const EditorTopbar = () => {
   const redo = useEditorStore((state) => state.redo);
   const histories = useEditorStore((state) => state.histories);
   const historyIdx = useEditorStore((state) => state.historyIdx);
+  const backHistories = useEditorStore((state) => state.backHistories);
+  const backHistoriesIdx = useEditorStore((state) => state.backHistoryIdx);
+  const isFront = useEditorStore((state) => state.isCanvasFront);
+
+  const currentHistories = isFront ? histories : backHistories;
+  const currentHistoriesIdx = isFront ? historyIdx : backHistoriesIdx;
 
   return (
     <div className='flex flex-row gap-4'>
       <button>재설정</button>
       <div>
-        <button onClick={undo} disabled={historyIdx < 1}>
+        <button onClick={undo} disabled={currentHistoriesIdx < 1}>
           undo
         </button>
-        <button onClick={redo} disabled={historyIdx === histories.length - 1}>
+        <button
+          onClick={redo}
+          disabled={currentHistoriesIdx === currentHistories.length - 1}
+        >
           redo
         </button>
       </div>
