89ad4e9436be85e3212ca3b921284b9596dffc4a
lyra <mk.lyra.j@gmail.com>
2025-04-16 04:25:24 +0900

    ♻️ refactor : 쿼리키 추가 및 훅 분리 #108

--- src/constants/query-key.ts
+++ src/constants/query-key.ts
@@ -1,6 +1,9 @@
 export const QUERY_KEY = {
   MONTH_SAVE: 'month-save',
+  MOST_VIEW: 'most-view',
   WEEK_CHART: 'week-chart',
+  MONTH_CHART: 'month-chart',
+  MONTH_STATS: 'month-stats',
   CLICK_TOTAL_CHART: 'click-total-chart',
   CARD_SELECT_LIST: 'card-select-list',
   CARD_INTERACTION: 'card-interaction',
