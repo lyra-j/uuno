19417e1155779d72c72fddc3eae7df8a02ba0a8a
K-jisu <wltn7star@naver.com>
2025-04-15 20:47:27 +0900

    🎉 add : 앞면 뒷면 버튼에 액션 추가 #104

--- src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
+++ src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
@@ -1,8 +1,23 @@
+import { useEditorStore } from '@/store/editor.store';
+
 const EditorBottomTab = () => {
+  const setCanvasFront = useEditorStore((state) => state.setCanvasFront);
   return (
     <div className='flex flex-row justify-center'>
-      <button>앞면</button>
-      <button>뒷면</button>
+      <button
+        onClick={() => {
+          setCanvasFront(true);
+        }}
+      >
+        앞면
+      </button>
+      <button
+        onClick={() => {
+          setCanvasFront(false);
+        }}
+      >
+        뒷면
+      </button>
     </div>
   );
 };
