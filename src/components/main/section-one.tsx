7bd9ed0c53d22f8d28f1b6c7f1a6302daf11c9df
woozizi <c980805@naver.com>
2025-04-17 06:01:43 +0900

    ♻️ refactor: section1 ui 수정  #157

--- src/components/main/section-one.tsx
+++ src/components/main/section-one.tsx
@@ -8,22 +8,22 @@ const Section1 = () => {
       style={{ backgroundImage: 'url(/main-bg.png' }}
     >
       {/*text*/}
-      <div className='z-1 relative flex flex-col'>
-        <h2 className='mb-4 text-5xl font-bold'>Uuno</h2>
-        <p className='mb-8 text-2xl'>
+      <div className='z-1 relative flex flex-col space-y-4'>
+        <h2 className='h-[59px] w-[153px] text-5xl font-bold'>Uuno</h2>
+        <p className='text-title-medium'>
           누구나 쉽게 만들고, <br />
           간편하게 공유하는 스마트 명함
         </p>
-        <div className='space-x-8'>
-          <button className='rounded-3xl bg-blue-500 px-6 py-3 font-bold text-white'>
-            바로 시작하기
+        <div className='flex flex-row gap-3'>
+          <button className='h-[56px] w-[162px] rounded-[46px] bg-primary-40 text-body-medium text-white'>
+            <p className='px-3 py-[6px] text-body-medium'>바로 시작하기</p>
           </button>
-          <button className='rounded-3xl bg-white px-6 py-3 font-bold'>
-            템플릿 보러가기
+          <button className='h-[56px] w-[162px] rounded-[46px] bg-white'>
+            <p className='px-3 py-[6px] text-body-medium'>템플릿 보러가기</p>
           </button>
         </div>
       </div>
-      <Image src='/main-image.png' alt='메인명함' width={630} height={840} />
+      <Image src='/main-image.png' alt='메인명함' width={1000} height={666} />
     </section>
   );
 };
