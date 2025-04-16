0ba737c29c6546d0ba09b42b013f70083aaf8d50
lyra <mk.lyra.j@gmail.com>
2025-04-16 14:36:02 +0900

    🩹 fix : 타이포그래피 줄간격 %에서 새로운 px 단위로 수정 #136

--- src/config/tailwind.typography.ts
+++ src/config/tailwind.typography.ts
@@ -8,48 +8,48 @@ const typography: Record<
   [string, { lineHeight: string; fontWeight: number }]
 > = {
   // ----------------- Landing -----------------
-  'landing-bold-l': ['40px', { lineHeight: '120%', fontWeight: 700 }],
-  'landing-bold-m': ['32px', { lineHeight: '125%', fontWeight: 700 }],
-  'landing-medium': ['28px', { lineHeight: '150%', fontWeight: 500 }],
+  'landing-bold-l': ['40px', { lineHeight: '48px', fontWeight: 700 }],
+  'landing-bold-m': ['32px', { lineHeight: '40px', fontWeight: 700 }],
+  'landing-title': ['28px', { lineHeight: '36px', fontWeight: 700 }],
+  'landing-medium': ['28px', { lineHeight: '36px', fontWeight: 500 }],
 
   // ----------------- Title -----------------
-  'title1-b': ['28px', { lineHeight: '150%', fontWeight: 700 }],
-  'title-bold': ['24px', { lineHeight: '150%', fontWeight: 700 }],
-  'title-medium': ['24px', { lineHeight: '150%', fontWeight: 500 }],
-  'title-regular': ['24px', { lineHeight: '150%', fontWeight: 400 }],
+  'title-bold': ['24px', { lineHeight: '32px', fontWeight: 700 }],
+  'title-medium': ['24px', { lineHeight: '32px', fontWeight: 500 }],
+  'title-regular': ['24px', { lineHeight: '32px', fontWeight: 400 }],
 
   // ----------------- Heading -----------------
-  'heading-bold': ['20px', { lineHeight: '150%', fontWeight: 700 }],
-  'heading-medium': ['20px', { lineHeight: '150%', fontWeight: 500 }],
-  'heading-semi': ['20px', { lineHeight: '150%', fontWeight: 600 }],
-  'heading-regular': ['20px', { lineHeight: '150%', fontWeight: 400 }],
+  'heading-bold': ['20px', { lineHeight: '30px', fontWeight: 700 }],
+  'heading-medium': ['20px', { lineHeight: '30px', fontWeight: 500 }],
+  'heading-semi': ['20px', { lineHeight: '30px', fontWeight: 600 }],
+  'heading-regular': ['20px', { lineHeight: '30px', fontWeight: 400 }],
 
   // ----------------- Body -----------------
-  'body-bold': ['18px', { lineHeight: '150%', fontWeight: 700 }],
-  'body-medium': ['18px', { lineHeight: '150%', fontWeight: 500 }],
-  'body-semi': ['18px', { lineHeight: '150%', fontWeight: 600 }],
-  'body-regular': ['18px', { lineHeight: '150%', fontWeight: 400 }],
+  'body-bold': ['18px', { lineHeight: '26px', fontWeight: 700 }],
+  'body-medium': ['18px', { lineHeight: '26px', fontWeight: 500 }],
+  'body-semi': ['18px', { lineHeight: '26px', fontWeight: 600 }],
+  'body-regular': ['18px', { lineHeight: '26px', fontWeight: 400 }],
 
   // ----------------- Label1 -----------------
-  'label1-bold': ['16px', { lineHeight: '150%', fontWeight: 700 }],
-  'label1-medium': ['16px', { lineHeight: '150%', fontWeight: 500 }],
-  'label1-semi': ['16px', { lineHeight: '150%', fontWeight: 600 }],
-  'label1-regular': ['16px', { lineHeight: '150%', fontWeight: 400 }],
+  'label1-bold': ['16px', { lineHeight: '24px', fontWeight: 700 }],
+  'label1-medium': ['16px', { lineHeight: '24px', fontWeight: 500 }],
+  'label1-semi': ['16px', { lineHeight: '24px', fontWeight: 600 }],
+  'label1-regular': ['16px', { lineHeight: '24px', fontWeight: 400 }],
 
   // ----------------- Label2 -----------------
-  'label2-bold': ['14px', { lineHeight: '150%', fontWeight: 700 }],
-  'label2-medium': ['14px', { lineHeight: '150%', fontWeight: 500 }],
-  'label2-semi': ['14px', { lineHeight: '150%', fontWeight: 600 }],
-  'label2-regular': ['14px', { lineHeight: '150%', fontWeight: 400 }],
+  'label2-bold': ['14px', { lineHeight: '20px', fontWeight: 700 }],
+  'label2-medium': ['14px', { lineHeight: '20px', fontWeight: 500 }],
+  'label2-semi': ['14px', { lineHeight: '20px', fontWeight: 600 }],
+  'label2-regular': ['14px', { lineHeight: '20px', fontWeight: 400 }],
 
   // ----------------- Extra -----------------
-  'extra-medium': ['13px', { lineHeight: '150%', fontWeight: 500 }],
+  'extra-medium': ['13px', { lineHeight: '18px', fontWeight: 500 }],
 
   // ----------------- Caption -----------------
-  'caption-bold': ['12px', { lineHeight: '150%', fontWeight: 700 }],
-  'caption-medium': ['12px', { lineHeight: '150%', fontWeight: 500 }],
-  'caption-semi': ['12px', { lineHeight: '150%', fontWeight: 600 }],
-  'caption-regular': ['12px', { lineHeight: '150%', fontWeight: 400 }],
+  'caption-bold': ['12px', { lineHeight: '16px', fontWeight: 700 }],
+  'caption-medium': ['12px', { lineHeight: '16px', fontWeight: 500 }],
+  'caption-semi': ['12px', { lineHeight: '16px', fontWeight: 600 }],
+  'caption-regular': ['12px', { lineHeight: '16px', fontWeight: 400 }],
 };
 
 export default typography;
