5300d0d186a0962b05fb8d9030c56d1bf02ed252
K-jisu <wltn7star@naver.com>
2025-04-17 06:52:16 +0900

    ✨update : 수직인 경우 카드 모양 변경 #162

--- src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
+++ src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
@@ -1,6 +1,7 @@
 import BottomTabDownIcon from '@/components/icons/editor/bottomtab-down';
 import BottomTabPolygonIcon from '@/components/icons/editor/bottomtab-polygon';
 import BottomTabUpIcon from '@/components/icons/editor/bottomtab-up';
+import { sideBarStore } from '@/store/editor.sidebar.store';
 import { useEditorStore } from '@/store/editor.store';
 import { useState } from 'react';
 
@@ -13,6 +14,7 @@ const EditorBottomTab = () => {
   const [bottomBarUp, setBottomBarUp] = useState(false);
   const isCanvasFront = useEditorStore((state) => state.isCanvasFront);
   const setisCanvasFront = useEditorStore((state) => state.setCanvasFront);
+  const isHorizontal = sideBarStore((state) => state.isHorizontal);
 
   return (
     <>
@@ -63,29 +65,43 @@ const EditorBottomTab = () => {
           </div>
         )}
         {bottomBarUp && (
-          <div className='flex h-[176px] flex-row items-center justify-center bg-white'>
-            <div className='z-10 flex h-[110px] w-[204px] items-center gap-3'>
+          <div className='flex h-[176px] items-center justify-center bg-white'>
+            <div
+              className={`z-10 flex ${
+                isHorizontal
+                  ? 'h-[110px] w-[240px] flex-row'
+                  : 'h-[150px] w-[150px] flex-row'
+              } items-center gap-3`}
+            >
               <div
-                className='g-1 flex h-full w-[96px] flex-col items-center'
+                className={`g-1 flex ${
+                  isHorizontal ? 'h-full w-[117px]' : 'h-[117px] w-full'
+                } flex-col items-center justify-center`}
                 onClick={() => setisCanvasFront(true)}
               >
                 <div
-                  className={`flex h-[70px] flex-1 items-center justify-center self-stretch border ${isCanvasFront ? 'border-black' : 'border-gray-20'} cursor-pointer p-[11px]`}
+                  className={`flex ${
+                    isHorizontal ? 'h-[61px] w-[117px]' : 'h-[117px] w-[61px]'
+                  } items-center justify-center border ${
+                    isCanvasFront ? 'border-black' : 'border-gray-20'
+                  } cursor-pointer p-[11px]`}
                 ></div>
-                <p className='flex flex-1 items-center text-caption-regular text-black'>
-                  앞면
-                </p>
+                <p className='text-caption-regular text-black'>앞면</p>
               </div>
               <div
-                className='g-1 flex h-full w-[96px] flex-col items-center'
+                className={`g-1 flex ${
+                  isHorizontal ? 'h-full w-[117px]' : 'h-[117px] w-full'
+                } flex-col items-center justify-center`}
                 onClick={() => setisCanvasFront(false)}
               >
                 <div
-                  className={`flex h-[70px] flex-1 items-center justify-center self-stretch border ${!isCanvasFront ? 'border-black' : 'border-gray-20'} cursor-pointer p-[11px]`}
+                  className={`flex ${
+                    isHorizontal ? 'h-[61px] w-[117px]' : 'h-[117px] w-[61px]'
+                  } items-center justify-center border ${
+                    !isCanvasFront ? 'border-black' : 'border-gray-20'
+                  } cursor-pointer p-[11px]`}
                 ></div>
-                <p className='flex flex-1 items-center text-caption-regular text-black'>
-                  뒷면
-                </p>
+                <p className='text-caption-regular text-black'>뒷면</p>
               </div>
             </div>
           </div>
