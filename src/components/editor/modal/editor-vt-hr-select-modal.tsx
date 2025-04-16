572612f1fd3dc5bb959fc49613167c37925d84e4
K-jisu <wltn7star@naver.com>
2025-04-16 20:01:29 +0900

    🩹 fix : 철자 오류 수정 #145

--- src/components/editor/modal/editor-vt-hr-select-modal.tsx
+++ src/components/editor/modal/editor-vt-hr-select-modal.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import CanvasHorizonIcon from '@/components/icons/editor/editor-horizon';
-import CanvasVerticleIcon from '@/components/icons/editor/editor-vericle';
+import CanvasVerticalIcon from '@/components/icons/editor/editor-vericle';
 import { useState } from 'react';
 
-const CanvasSelectMadal = () => {
+const CanvasSelectModal = () => {
   const [openModal, setOpenModal] = useState(true);
   if (!openModal) {
     return null;
@@ -30,7 +30,7 @@ const CanvasSelectMadal = () => {
           </div>
           <div className='group flex h-[126px] w-[104px] flex-shrink-0 cursor-pointer flex-col items-center justify-center rounded-sm border border-gray-10 bg-white px-6 py-[19px] hover:border-primary-40'>
             <div className='flex w-14 flex-col items-center gap-[16px]'>
-              <CanvasVerticleIcon className='h-[56px] w-[56px] text-gray-70 group-hover:text-primary-40' />
+              <CanvasVerticalIcon className='h-[56px] w-[56px] text-gray-70 group-hover:text-primary-40' />
               <p className='text-center text-caption-medium text-gray-70 group-hover:text-primary-40'>
                 세로형태
               </p>
@@ -42,4 +42,4 @@ const CanvasSelectMadal = () => {
   );
 };
 
-export default CanvasSelectMadal;
+export default CanvasSelectModal;
