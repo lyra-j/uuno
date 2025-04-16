cdcabb070a4d5d9e0600bc1e6202951334afd59f
K-jisu <wltn7star@naver.com>
2025-04-17 03:57:48 +0900

    🎉add : Auth에 사용하는 svg 추가 #140

---
+++ src/components/icons/auth/auth-confirm.tsx
@@ -0,0 +1,24 @@
+const AuthConfirmIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='16'
+    height='16'
+    viewBox='0 0 16 16'
+    fill='none'
+    {...props}
+  >
+    <g clip-path='url(#clip0_1125_26870)'>
+      <path
+        d='M8.00033 15.3334C12.0503 15.3334 15.3337 12.05 15.3337 8.00002C15.3337 3.95002 12.0503 0.666687 8.00033 0.666687C3.95033 0.666687 0.666992 3.95002 0.666992 8.00002C0.666992 12.05 3.95033 15.3334 8.00033 15.3334ZM5.00033 7.05735L7.00033 9.05735L11.0003 5.05735L11.943 6.00002L7.00033 10.9427L4.05766 8.00002L5.00033 7.05735Z'
+        fill='#3970D5'
+      />
+    </g>
+    <defs>
+      <clipPath id='clip0_1125_26870'>
+        <rect width='16' height='16' fill='white' />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default AuthConfirmIcon;
