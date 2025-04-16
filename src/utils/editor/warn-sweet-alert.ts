e04e104290fbaa90da60b1efdd2b2d827772f8a0
K-jisu <wltn7star@naver.com>
2025-04-16 22:32:26 +0900

    🎉add : 변경시 작업내용 날라간다는 sweetalert 함수 #105

---
+++ src/utils/editor/warn-sweet-alert.ts
@@ -0,0 +1,41 @@
+'use client';
+import { sideBarStore } from '@/store/editor.sidebar.store';
+import { useEditorStore } from '@/store/editor.store';
+import Swal from 'sweetalert2';
+
+export const handleSwitchCard = () => {
+  const { canvasElements, canvasBackElements, isCanvasFront, reset } =
+    useEditorStore.getState();
+
+  const { isHorizontal, setIsHorizontal, setZoom } = sideBarStore.getState();
+
+  const currentCanvasElements = isCanvasFront
+    ? canvasElements
+    : canvasBackElements;
+  if (currentCanvasElements.length > 0) {
+    Swal.fire({
+      title: '변경하시겠습니까?',
+      text: '변경하면 현재 작업하신 내용은 삭제가 됩니다. 진행하시겠습니까?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '변경하겠습니다!',
+      cancelButtonText: '취소',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '변경되었습니다!',
+          text: '',
+          icon: 'success',
+        });
+        reset();
+        setIsHorizontal(!isHorizontal);
+        isHorizontal ? setZoom(1.4) : setZoom(2);
+      }
+    });
+  } else {
+    setIsHorizontal(!isHorizontal);
+    isHorizontal ? setZoom(1.4) : setZoom(2);
+  }
+};
