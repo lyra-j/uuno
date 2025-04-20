1f7d89dcba3cc26b13b4aa6a0259089c1609dc18
K-jisu <wltn7star@naver.com>
2025-04-21 00:54:52 +0900

    ♻️ refactor : isCardContent 함수의 검증 로직 개선하여 다양한 속성 확인 기능 추가 #204

--- src/utils/editor/editor-card-type-guard.ts
+++ src/utils/editor/editor-card-type-guard.ts
@@ -2,6 +2,12 @@ import { CardContent } from '@/types/editor.type';
 
 export const isCardContent = (content: unknown): content is CardContent => {
   return (
-    typeof content === 'object' && content !== null && 'isTemplate' in content
+    typeof content === 'object' &&
+    content !== null &&
+    ('canvasElements' in content ||
+      'canvasBackElements' in content ||
+      'backgroundColor' in content ||
+      'backgroundColorBack' in content ||
+      'isTemplate' in content)
   );
 };
