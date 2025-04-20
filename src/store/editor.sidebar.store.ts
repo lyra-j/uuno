257e38034fe14b1cd3c725d8bb33d232df6f76ae
K-jisu <wltn7star@naver.com>
2025-04-20 22:38:46 +0900

    ♻️ refactor : 'isTemplate' 속성과 관련 setter 추가 #204

--- src/store/editor.sidebar.store.ts
+++ src/store/editor.sidebar.store.ts
@@ -5,10 +5,12 @@ interface SideBar {
   zoom: number;
   isSocialEditing: boolean;
   isHorizontal: boolean;
+  isTemplate: boolean;
   setSideBarStatus: (status: boolean) => void;
   setZoom: (zoom: number) => void;
   setIsSocialEditing: (status: boolean) => void;
   setIsHorizontal: (status: boolean) => void;
+  setIsTemplate: (status: boolean) => void;
 }
 
 export const sideBarStore = create<SideBar>()((set) => ({
@@ -23,4 +25,7 @@ export const sideBarStore = create<SideBar>()((set) => ({
 
   isHorizontal: true,
   setIsHorizontal: (status) => set({ isHorizontal: status }),
+
+  isTemplate: false,
+  setIsTemplate: (status) => set({ isTemplate: status }),
 }));
