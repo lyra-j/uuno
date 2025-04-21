f22abbf7acc66222e78205e373e82ec2a5f0e11c
K-jisu <wltn7star@naver.com>
2025-04-21 15:03:22 +0900

    🎉 add : 미리보기 캔버스 설정 #214

--- src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
+++ src/components/editor/editor-ui/bottomTab/editor-bottom-tab.tsx
@@ -1,9 +1,28 @@
+import { SwitchCase } from '@/components/common/switch-case';
 import BottomTabDownIcon from '@/components/icons/editor/bottomtab-down';
 import BottomTabPolygonIcon from '@/components/icons/editor/bottomtab-polygon';
 import BottomTabUpIcon from '@/components/icons/editor/bottomtab-up';
 import { sideBarStore } from '@/store/editor.sidebar.store';
 import { useEditorStore } from '@/store/editor.store';
 import { useState } from 'react';
+import { Layer, Rect, Stage } from 'react-konva';
+import {
+  ImageElement,
+  QrElement,
+  SocialElement,
+  TextElement,
+  UploadElement,
+} from '@/types/editor.type';
+import {
+  BASE_STAGE_HEIGHT,
+  BASE_STAGE_WIDTH,
+  ElEMENT_TYPE,
+} from '@/constants/editor.constant';
+import PreviewTextCanvas from '../../elements/text/preview-text-canvas';
+import PreviewUploadCanvas from '../../elements/uploads/preview-upload-canvas';
+import PreviewImageCanvas from '../../elements/images/preview-image-canvas';
+import PreviewQrCanvas from '../../elements/qr-social/preview-qr-canvas';
+import PreviewSocialCanvas from '../../elements/qr-social/preview-social-canvas';
 
 const EditorBottomTab = () => {
   const setCanvasFront = useEditorStore((state) => state.setCanvasFront);
@@ -15,6 +34,13 @@ const EditorBottomTab = () => {
   const isCanvasFront = useEditorStore((state) => state.isCanvasFront);
   const setisCanvasFront = useEditorStore((state) => state.setCanvasFront);
   const isHorizontal = sideBarStore((state) => state.isHorizontal);
+  const canvasElement = useEditorStore((state) => state.canvasElements);
+
+  const stageWidth = BASE_STAGE_WIDTH;
+  const stageHeight = BASE_STAGE_HEIGHT;
+
+  const currentStageWidth = isHorizontal ? stageWidth : stageHeight;
+  const currentStageHeight = isHorizontal ? stageHeight : stageWidth;
 
   return (
     <>
@@ -85,7 +111,57 @@ const EditorBottomTab = () => {
                   } items-center justify-center border ${
                     isCanvasFront ? 'border-black' : 'border-gray-20'
                   } cursor-pointer p-[11px]`}
-                ></div>
+                >
+                  <Stage
+                    width={isHorizontal ? 117 : 61}
+                    height={isHorizontal ? 61 : 117}
+                    scale={{
+                      x: (isHorizontal ? 117 : 61) / currentStageWidth,
+                      y: (isHorizontal ? 61 : 117) / currentStageHeight,
+                    }}
+                  >
+                    <Layer>
+                      <Rect
+                        x={0}
+                        y={0}
+                        width={isHorizontal ? 117 : 61}
+                        height={isHorizontal ? 61 : 117}
+                        // fill={backgroundColor}
+                        listening={false}
+                      />
+                      {canvasElement.map((el) => (
+                        <SwitchCase
+                          key={el.id}
+                          value={el.type}
+                          cases={{
+                            [ElEMENT_TYPE.TEXT]: (
+                              <PreviewTextCanvas element={el as TextElement} />
+                            ),
+                            [ElEMENT_TYPE.UPLOAD]: (
+                              <PreviewUploadCanvas
+                                element={el as UploadElement}
+                              />
+                            ),
+                            [ElEMENT_TYPE.IMAGE]: (
+                              <PreviewImageCanvas
+                                element={el as ImageElement}
+                              />
+                            ),
+                            [ElEMENT_TYPE.QR]: (
+                              <PreviewQrCanvas element={el as QrElement} />
+                            ),
+                            [ElEMENT_TYPE.SOCIAL]: (
+                              <PreviewSocialCanvas
+                                element={el as SocialElement}
+                              />
+                            ),
+                          }}
+                          default={<></>}
+                        />
+                      ))}
+                    </Layer>
+                  </Stage>
+                </div>
                 <p className='text-caption-regular text-black'>앞면</p>
               </div>
               <div
