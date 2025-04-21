a53c027e9ea5a43a6023984c05dc02060da886c7
woozizi <c980805@naver.com>
2025-04-22 01:54:26 +0900

    📄 docs : 주석 추가  #199

--- src/components/editor/canvas-elements-render.tsx
+++ src/components/editor/canvas-elements-render.tsx
@@ -18,6 +18,7 @@ import QrCanvasElement from '@/components/editor/elements/qr-social/element-qr-c
 import SocialCanvasElement from '@/components/editor/elements/qr-social/element-social-canvas';
 import { SwitchCase } from '../common/switch-case';
 import { ElEMENT_TYPE, TOOLBAR_WIDTH } from '@/constants/editor.constant';
+import { useCallback } from 'react';
 
 interface CanvasElementsRenderProps {
   elements: CanvasElements[];
@@ -60,26 +61,25 @@ const CanvasElementsRender = ({
   /**
    * 변환 종료 시 업데이트
    */
-  const handleTransformEnd = (
-    id: string,
-    e: Konva.KonvaEventObject<Event>
-  ): void => {
-    const node = e.target;
-    const scaleX = node.scaleX();
-    const scaleY = node.scaleY();
-    node.scaleX(1);
-    node.scaleY(1);
-
-    updateElement(id, {
-      x: node.x(),
-      y: node.y(),
-      width: Math.max(10, node.width() * scaleX),
-      height: Math.max(10, node.height() * scaleY),
-      rotation: node.rotation(),
-    });
-
-    handleUpdateToolbarNode(node);
-  };
+  const handleTransformEnd = useCallback(
+    (id: string, e: Konva.KonvaEventObject<Event>): void => {
+      const node = e.target;
+      if (!node) return;
+      const scaleX = node.scaleX();
+      const scaleY = node.scaleY();
+      node.scaleX(1);
+      node.scaleY(1);
+      updateElement(id, {
+        x: node.x(),
+        y: node.y(),
+        width: Math.max(10, node.width() * scaleX),
+        height: Math.max(10, node.height() * scaleY),
+        rotation: node.rotation(),
+      });
+      handleUpdateToolbarNode(node);
+    },
+    [updateElement, handleUpdateToolbarNode]
+  );
 
   /**
    * 더블 클릭 시 편집 모드 실행 핸들러
@@ -90,70 +90,66 @@ const CanvasElementsRender = ({
     setEditingElementId(id);
   };
 
-  return (
-    <>
-      {elements.map((el) => {
-        const commonProps = {
-          onDragEnd: (id: string, node: Konva.Node) => {
-            updateElement(id, { x: node.x(), y: node.y() });
-            handleUpdateToolbarNode(node);
-          },
-          onDragMove: (node: Konva.Node) => {
-            handleUpdateToolbarNode(node);
-          },
-          onTransformEnd: handleTransformEnd,
-          onSelect: (id: string, node: Konva.Node) => {
-            setSelectedElementId(id);
-            handleUpdateToolbarNode(node);
-            setSelectedElementType(el.type);
-            setSideBarStatus(true);
-          },
-          ref: (node: Konva.Node | null) => {
-            if (node) shapeRefs.current[el.id] = node;
-          },
-        };
+  return elements.map((el) => {
+    const commonProps = {
+      onDragEnd: (id: string, node: Konva.Node) => {
+        updateElement(id, { x: node.x(), y: node.y() });
+        handleUpdateToolbarNode(node);
+      },
+      onDragMove: (node: Konva.Node) => {
+        handleUpdateToolbarNode(node);
+      },
+      onTransformEnd: handleTransformEnd,
+      onSelect: (id: string, node: Konva.Node) => {
+        setSelectedElementId(id);
+        handleUpdateToolbarNode(node);
+        setSelectedElementType(el.type);
+        setSideBarStatus(true);
+      },
+      ref: (node: Konva.Node | null) => {
+        if (node) shapeRefs.current[el.id] = node;
+      },
+    };
 
-        return (
-          <SwitchCase
-            key={el.id}
-            value={el.type}
-            cases={{
-              [ElEMENT_TYPE.TEXT]: (
-                <TextCanvasElement
-                  element={el as TextElement}
-                  {...commonProps}
-                  onDoubleClick={handleTextDoubleClick}
-                  editing={editingElementId === el.id}
-                />
-              ),
-              [ElEMENT_TYPE.UPLOAD]: (
-                <UploadImageElement
-                  element={el as UploadElement}
-                  {...commonProps}
-                />
-              ),
-              [ElEMENT_TYPE.IMAGE]: (
-                <UnsplashImageElement
-                  element={el as ImageElement}
-                  {...commonProps}
-                />
-              ),
-              [ElEMENT_TYPE.QR]: (
-                <QrCanvasElement element={el as QrElement} {...commonProps} />
-              ),
-              [ElEMENT_TYPE.SOCIAL]: (
-                <SocialCanvasElement
-                  element={el as SocialElement}
-                  {...commonProps}
-                />
-              ),
-            }}
-            default={<></>}
-          />
-        );
-      })}
-    </>
-  );
+    return (
+      <SwitchCase
+        key={el.id}
+        value={el.type}
+        cases={{
+          [ElEMENT_TYPE.TEXT]: (
+            <TextCanvasElement
+              element={el as TextElement}
+              {...commonProps}
+              onDoubleClick={handleTextDoubleClick}
+              editing={editingElementId === el.id}
+            />
+          ),
+          [ElEMENT_TYPE.UPLOAD]: (
+            <UploadImageElement
+              element={el as UploadElement}
+              {...commonProps}
+            />
+          ),
+          [ElEMENT_TYPE.IMAGE]: (
+            <UnsplashImageElement
+              element={el as ImageElement}
+              {...commonProps}
+            />
+          ),
+          [ElEMENT_TYPE.QR]: (
+            <QrCanvasElement element={el as QrElement} {...commonProps} />
+          ),
+          [ElEMENT_TYPE.SOCIAL]: (
+            <SocialCanvasElement
+              element={el as SocialElement}
+              {...commonProps}
+            />
+          ),
+        }}
+        default={<></>}
+      />
+    );
+  });
 };
 
 export default CanvasElementsRender;
