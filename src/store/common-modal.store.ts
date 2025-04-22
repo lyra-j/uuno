066ab2ffd8cf3512425ef9cb5ff639ef376f1687
노수민 <nosumin29@gmail.com>
2025-04-23 03:07:00 +0900

    🎉 add : 공통 모달 상태관리 스토어 추가 #234

--- src/store/common-modal.store.ts
+++ src/store/common-modal.store.ts
@@ -1,13 +1,49 @@
 import { create } from 'zustand';
+import { ReactNode } from 'react';
 
-interface ModalState {
+interface CommonModalState {
   isOpen: boolean;
-  open: () => void;
+  title: string;
+  description?: string;
+  content: ReactNode | null;
+  footer?: ReactNode;
+  maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
+  ctnClassName?: string;
+  onClose?: () => void;
+
+  open: (params: {
+    title: string;
+    description?: string;
+    content: ReactNode;
+    footer?: ReactNode;
+    maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
+    ctnClassName?: string;
+    onClose?: () => void;
+  }) => void;
   close: () => void;
 }
 
-export const useCommonModalStore = create<ModalState>((set) => ({
+export const useCommonModalStore = create<CommonModalState>((set) => ({
   isOpen: false,
-  open: () => set({ isOpen: true }),
+  title: '',
+  description: undefined,
+  content: null,
+  footer: undefined,
+  maxWidth: 'md',
+  ctnClassName: '',
+  onClose: undefined,
+
+  open: (params) =>
+    set({
+      isOpen: true,
+      title: params.title,
+      description: params.description,
+      content: params.content,
+      footer: params.footer,
+      maxWidth: params.maxWidth,
+      ctnClassName: params.ctnClassName,
+      onClose: params.onClose,
+    }),
+
   close: () => set({ isOpen: false }),
 }));
