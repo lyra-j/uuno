fac17fa250c34e99408ee8834044f14a129e6785
woozizi <c980805@naver.com>
2025-04-22 21:06:22 +0900

    ♻️ refactor : console 제거 #227

--- src/components/editor/elements/uploads/element-upload-canvas.tsx
+++ src/components/editor/elements/uploads/element-upload-canvas.tsx
@@ -21,10 +21,10 @@ const UploadImageElement = forwardRef<Konva.Image, UploadImageElementProps>(
   ) => {
     const [image, status] = useImage(element.previewUrl, 'anonymous');
 
-    // 로딩 실패 시 콘솔에 찍어줍니다.
+    //@TODO sweetalert로 대체
     useEffect(() => {
       if (status === 'failed') {
-        console.error(`이미지 로드 실패 CORS?: ${element.previewUrl}`);
+        alert(`이미지 로드 실패 CORS?: ${element.previewUrl}`);
       }
     }, [status, element.previewUrl]);
 
