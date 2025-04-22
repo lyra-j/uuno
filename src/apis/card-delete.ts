a4a856b1abb8ec0eea6fac0b72d78f9a1a0e635f
K-jisu <wltn7star@naver.com>
2025-04-22 17:50:41 +0900

    🐛 bugfix : 내 카드 삭제할 때 해당 storage image 삭제 #243

--- src/apis/card-delete.ts
+++ src/apis/card-delete.ts
@@ -1,14 +1,25 @@
+'use server';
 import { DB_COLUMNS, TABLES } from '@/constants/tables.constant';
-import { createClient } from '@/utils/supabase/client';
+import { createClient } from '@/utils/supabase/server';
+import { deleteAllFilesInFolder } from './storage';
 
-export const deleteCard = async (card_id: string) => {
+interface Props {
+  slug: string;
+  cardId: string;
+  userId: string;
+}
+
+export const deleteCard = async ({ cardId, slug, userId }: Props) => {
   const supabase = await createClient();
   const { data, error } = await supabase
     .from(TABLES.CARDS)
     .delete()
-    .eq(DB_COLUMNS.CARDS.ID, card_id);
+    .eq(DB_COLUMNS.CARDS.ID, cardId);
 
   if (error) throw error;
+
+  const folderPath = `${userId}/${slug}`;
+  await deleteAllFilesInFolder('cards', folderPath);
 
   return data;
 };
