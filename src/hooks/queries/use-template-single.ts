37c00bfa1cd74444a84b47d9fd151e3d3328623f
K-jisu <wltn7star@naver.com>
2025-04-23 23:53:47 +0900

    ✨ update : Template 타입 변경 #277

--- src/hooks/queries/use-template-single.ts
+++ src/hooks/queries/use-template-single.ts
@@ -1,13 +1,13 @@
 import { getSingleData } from '@/apis/common-server.api';
 import { QUERY_KEY } from '@/constants/query-key';
-import { Templates } from '@/types/supabase.type';
+import { TemplateData } from '@/types/templates.type';
 import { useQuery } from '@tanstack/react-query';
 
 export const getTemplateData = (templateId: string) => {
   return useQuery({
     queryKey: [QUERY_KEY.TEMPLATES, templateId],
     queryFn: async () => {
-      const { data } = await getSingleData<Templates>({
+      const { data } = await getSingleData<TemplateData>({
         table: 'templates',
         field: 'id',
         value: templateId,
