7d54a340721e3c89e57e9b08cc6e777a647b569e
lyra <mk.lyra.j@gmail.com>
2025-04-23 15:10:54 +0900

    🩹 fix : 한 화면에 보이도록 단위 수정 #264

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -8,7 +8,7 @@ const AccountPage = () => {
   return (
     <article className='rounded-xl bg-white px-12 py-8'>
       {/* 기본 정보 섹션 */}
-      <section className='flex h-[calc(100vh-14rem)] w-full items-center'>
+      <section className='flex h-[calc(100vh-16rem)] w-full items-center'>
         <h3 className='flex w-full items-center justify-center pb-2 text-center text-body-medium text-black'>
           계정 설정 페이지는 <br />
           준비 중입니다.
