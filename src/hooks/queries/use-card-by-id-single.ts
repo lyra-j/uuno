362b49b144005fa720bc312d86f9692244a5206a
woozizi <c980805@naver.com>
2025-04-24 21:55:41 +0900

    ♻️ refactor : 상수화 처리  #281

--- src/hooks/queries/use-card-by-id-single.ts
+++ src/hooks/queries/use-card-by-id-single.ts
@@ -1,13 +1,15 @@
 import { getSingleDataByClient } from '@/apis/common-client.api';
+import { QUERY_KEY } from '@/constants/query-key';
+import { TABLES } from '@/constants/tables.constant';
 import { CardData } from '@/types/cards.type';
 import { useQuery } from '@tanstack/react-query';
 
 export const useCardDataById = (cardId: string) =>
   useQuery({
-    queryKey: ['card', cardId],
+    queryKey: [QUERY_KEY.CARD, cardId],
     queryFn: async () => {
       const { data, error } = await getSingleDataByClient<CardData>({
-        table: 'cards',
+        table: TABLES.CARDS,
         field: 'id',
         value: cardId,
       });
