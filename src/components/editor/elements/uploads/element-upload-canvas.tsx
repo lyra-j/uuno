698727b43f939c699e8262614fc2a985db0ac44b
노수민 <nosumin29@gmail.com>
2025-04-25 13:29:57 +0900

    🩹 fix : 스윗얼럿 원복 #312

--- src/components/editor/elements/uploads/element-upload-canvas.tsx
+++ src/components/editor/elements/uploads/element-upload-canvas.tsx
@@ -4,6 +4,7 @@ import { Image as KonvaImage, Group, Rect, Text } from 'react-konva';
 import Konva from 'konva';
 import { UploadElement } from '@/types/editor.type';
 import { useImage } from 'react-konva-utils';
+import sweetAlertUtil from '@/utils/common/sweet-alert-util';
 
 interface UploadImageElementProps {
   element: UploadElement;
@@ -35,7 +36,7 @@ const UploadImageElement = forwardRef<Konva.Image, UploadImageElementProps>(
       // 이미지 로드 실패 시 에러 상태로 설정
       if (status === 'failed') {
         setHasError(true);
-        console.warn(`이미지를 불러올 수 없습니다: ${element.previewUrl}`);
+        sweetAlertUtil.error('이미지를 불러올 수 없습니다.');
       } else {
         setHasError(false);
       }
