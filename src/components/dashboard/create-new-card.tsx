cd88993d6e0b715d5ba29dbf82570e1ba14fd4d5
lyra <mk.lyra.j@gmail.com>
2025-04-27 07:58:49 +0900

    ♻️ refactor : 보유한 명함 갯수 Props 전달 형태로 변경 #324 모바일에서 카드 갯수가 1개 이상이면 만들기 카드 안보이게 하기

--- src/components/dashboard/create-new-card.tsx
+++ src/components/dashboard/create-new-card.tsx
@@ -1,41 +1,17 @@
 'use client';
 
 import { ROUTES } from '@/constants/path.constant';
-import { getCardCount } from '@/apis/card-interaction';
-import { createClient } from '@/utils/supabase/client';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Icon } from '@iconify/react';
 import sweetAlertUtil from '@/utils/common/sweet-alert-util';
 
-const CreateNewCard = () => {
-  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+interface CreateNewCardProps {
+  cardCount: number;
+}
 
-  /**
-   * 컴포넌트 마운트 시 한 번 실행:
-   * Supabase로부터 현재 로그인한 사용자를 조회하고,
-   * getCardCount 호출하여 사용자가 생성한 명함 개수를 가져옴
-   * 개수가 3개 이상이면 버튼을 비활성화 상태로 설정
-   */
-  useEffect(() => {
-    const checkCardCount = async () => {
-      const supabase = createClient();
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-
-      if (user) {
-        try {
-          const count = await getCardCount(user.id);
-          setIsDisabled(count >= 3);
-        } catch (error: any) {
-          console.error('명함 개수 조회 중 오류 발생 : ', error);
-        }
-      }
-    };
-
-    checkCardCount();
-  }, []);
+const CreateNewCard = ({ cardCount }: CreateNewCardProps) => {
+  const isDisabled = cardCount >= 3;
 
   /**
    * 버튼 클릭 핸들러
@@ -56,7 +32,7 @@ const CreateNewCard = () => {
     <Link
       href={ROUTES.EDITOR}
       onClick={handleClick}
-      className={`flex aspect-[9/5] w-full cursor-pointer items-center justify-center rounded-xl border border-dashed border-gray-40 transition-colors ${
+      className={`flex aspect-[9/5] w-full cursor-pointer items-center justify-center rounded-xl border border-dashed border-gray-40 transition-colors ${cardCount >= 1 ? 'max-md:hidden' : ''} ${
         isDisabled
           ? 'cursor-not-allowed opacity-50' // 활성화 스타일
           : 'hover:bg-gray-10' // 활성화 hover
