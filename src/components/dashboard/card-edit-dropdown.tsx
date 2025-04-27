6cf212651a6ad1a81a08b74d246d3c5a3de45577
lyra <mk.lyra.j@gmail.com>
2025-04-28 01:27:17 +0900

    💄 style : 드롭다운 메뉴 md 기준 visibility 처리 완료 #329

--- src/components/dashboard/card-edit-dropdown.tsx
+++ src/components/dashboard/card-edit-dropdown.tsx
@@ -99,74 +99,6 @@ const CardEditDropdown = ({
     });
   };
 
-  // 공통 메뉴 아이템 JSX
-  const MenuItems = (
-    <>
-      {/* 드롭다운 라벨 */}
-      {/* --- 메뉴 제일 위: 명함 제목과 수정 버튼 --- */}
-      <DropdownMenuLabel className='flex flex-col'>
-        <div className='flex items-center justify-between text-label1-bold'>
-          {title}
-          <button
-            type='button'
-            onClick={(e) => {
-              e.stopPropagation();
-              e.preventDefault();
-              onEdit(); // 제목 편집모드 전환
-
-              setTimeout(() => {
-                setOpen(false); // 편집 시작할 때 드롭다운 닫기
-              }, 10);
-            }}
-          >
-            <PencilIcon />
-          </button>
-        </div>
-        <div className='mt-1 text-caption-medium text-gray-60'>{dateLabel}</div>
-      </DropdownMenuLabel>
-
-      {/* 구분선 */}
-      <DropdownMenuSeparator className='my-2' />
-      {/* 드롭다운 목록 */}
-
-      {/* --- 저장 및 공유하기 --- 모달 */}
-      <DropdownMenuItem onClick={handleOpenShareModal}>
-        <Icon icon='tdesign:save' width='16' height='16' />
-        저장 및 공유하기
-      </DropdownMenuItem>
-
-      {/* --- 삭제하기 --- */}
-      <DropdownMenuItem onClick={handleDeleteCard} disabled={isDeleting}>
-        <DeleteIcon />
-        {isDeleting ? '삭제 중...' : '삭제하기'}
-      </DropdownMenuItem>
-
-      {/* --- 통계보기 --- */}
-      <DropdownMenuItem asChild>
-        <Link href={`${ROUTES.MYCARD}/${cardId}`}>
-          <Icon icon='tdesign:leaderboard' width='18' height='18' />
-          통계보기
-        </Link>
-      </DropdownMenuItem>
-
-      {/* --- 편집하기 : 에디터 --- */}
-      <DropdownMenuItem asChild>
-        <Link href={`${ROUTES.EDITOR}?cardId=${cardId}`}>
-          <Icon icon='tdesign:edit-2' width='18' height='18' />
-          편집하기
-        </Link>
-      </DropdownMenuItem>
-
-      {/* --- 공유화면 미리보기 --- */}
-      <DropdownMenuItem asChild>
-        <Link href={`/${slug}`}>
-          <PreviewIcon />
-          공유 화면 미리보기
-        </Link>
-      </DropdownMenuItem>
-    </>
-  );
-
   return (
     <div className='group relative z-50'>
       {/* --- 데스크탑(md이상) : hover시 버튼 표시, 클릭시 드롭다운 --- */}
@@ -190,16 +122,96 @@ const CardEditDropdown = ({
           </DropdownMenuTrigger>
 
           {/* 드롭다운 메뉴 내용 */}
+          {/* TODO: 왜 명함마다 드롭다운 메뉴가 뜨는 위치가 다른지, 확인 필요 */}
           <DropdownMenuContent
             side='bottom'
-            align='start'
+            align='end'
             className='z-50 w-[206px]'
           >
-            {/* 드롭다운 라벨부터, 공유 미리보기 까지 */}
-            {MenuItems}
+            {/* 드롭다운 라벨 */}
+            {/* --- 메뉴 제일 위: 명함 제목과 수정 버튼 --- */}
+            <DropdownMenuLabel className='flex flex-col'>
+              <div className='flex items-center justify-between text-label1-bold'>
+                {title}
+                <button
+                  type='button'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    onEdit(); // 제목 편집모드 전환
+
+                    setTimeout(() => {
+                      setOpen(false); // 편집 시작할 때 드롭다운 닫기
+                    }, 10);
+                  }}
+                >
+                  <PencilIcon />
+                </button>
+              </div>
+              <div className='mt-1 text-caption-medium text-gray-60'>
+                {dateLabel}
+              </div>
+            </DropdownMenuLabel>
+
+            {/* 구분선 */}
+            <DropdownMenuSeparator className='my-2' />
+            {/* 드롭다운 목록 */}
+
+            {/* --- 저장 및 공유하기 --- 모달 */}
+            <DropdownMenuItem onClick={handleOpenShareModal}>
+              <Icon icon='tdesign:save' width='16' height='16' />
+              저장 및 공유하기
+            </DropdownMenuItem>
+
+            {/* --- 삭제하기 --- */}
+            <DropdownMenuItem onClick={handleDeleteCard} disabled={isDeleting}>
+              <DeleteIcon />
+              {isDeleting ? '삭제 중...' : '삭제하기'}
+            </DropdownMenuItem>
+
+            {/* --- 통계보기 --- */}
+            <DropdownMenuItem asChild>
+              <Link href={`${ROUTES.MYCARD}/${cardId}`}>
+                <Icon icon='tdesign:leaderboard' width='18' height='18' />
+                통계보기
+              </Link>
+            </DropdownMenuItem>
+
+            {/* --- 편집하기 : 에디터 --- */}
+            <DropdownMenuItem asChild>
+              <Link href={`${ROUTES.EDITOR}?cardId=${cardId}`}>
+                <Icon icon='tdesign:edit-2' width='18' height='18' />
+                편집하기
+              </Link>
+            </DropdownMenuItem>
+
+            {/* --- 공유화면 미리보기 --- */}
+            <DropdownMenuItem asChild>
+              <Link href={`/${slug}`}>
+                <PreviewIcon />
+                공유 화면 미리보기
+              </Link>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
+
+      {/* --- 모바일(md이하) : 트리거 버튼 항상 보이게, 바텀 시트 메뉴 --- */}
+      <button
+        type='button'
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+        aria-label='카드 메뉴 열기'
+        aria-haspopup='true'
+        aria-expanded={open}
+        className='absolute right-[10px] top-[10px] flex h-8 w-8 items-center justify-center rounded-full border border-gray-5 bg-white md:hidden'
+      >
+        <Ellipsis />
+      </button>
+
+      {/* 바텀 시트 컨테이너 */}
     </div>
   );
 };
