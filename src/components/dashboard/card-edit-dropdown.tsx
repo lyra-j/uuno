25dc53160f4369846d71eed9eb1e9bc98f8ce245
lyra <mk.lyra.j@gmail.com>
2025-04-28 00:15:17 +0900

    ♻️ refactor : 코드 정리 및 반응형 대비 공통 메뉴 분리 #329

--- src/components/dashboard/card-edit-dropdown.tsx
+++ src/components/dashboard/card-edit-dropdown.tsx
@@ -41,19 +41,24 @@ const CardEditDropdown = ({
   slug,
   imageUrl,
 }: Props) => {
-  // 드롭다운 상태 관리 추가
+  // 드롭다운 상태 관리
   const [open, setOpen] = useState(false);
-  // 삭제 중 상태 관리 추가
+  // 삭제 처리중 상태 관리
   const [isDeleting, setIsDeleting] = useState(false);
-
+  // 현재 호스트(origin) 저장
   const [origin, setOrigin] = useState<string>('');
+  // 사용자 닉네임 조회
+  const nickName = authStore((state) => state.userName);
+  // 저장 및 공유 모달 오픈
+  const openShareModal = useSaveShareModalStore((state) => state.open);
+  // 명함 삭제 뮤테이션훅
+  const { mutate: deleteMutate } = useMyCardDelete({ slug, cardId, userId });
+
+  // 최초 렌더시 origin 설정
   useEffect(() => {
     if (typeof window !== 'undefined') setOrigin(window.location.origin);
   }, []);
 
-  const nickName = authStore((state) => state.userName);
-
-  const openShareModal = useSaveShareModalStore((state) => state.open);
   const handleOpenShareModal = () => {
     if (slug) {
       openShareModal({
@@ -66,8 +71,7 @@ const CardEditDropdown = ({
     }
   };
 
-  const { mutate: deleteMutate } = useMyCardDelete({ slug, cardId, userId });
-
+  // 명함 삭제 핸들러
   const handleDeleteCard = async (e: React.MouseEvent) => {
     e.stopPropagation();
     e.preventDefault();
@@ -96,6 +100,74 @@ const CardEditDropdown = ({
     });
   };
 
+  // 공통 메뉴 아이템 JSX
+  const MenuItems = (
+    <>
+      {/* 드롭다운 라벨 */}
+      {/* --- 메뉴 제일 위: 명함 제목과 수정 버튼 --- */}
+      <DropdownMenuLabel className='flex flex-col'>
+        <div className='flex items-center justify-between text-label1-bold'>
+          {title}
+          <button
+            type='button'
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              onEdit(); // 제목 편집모드 전환
+
+              setTimeout(() => {
+                setOpen(false); // 편집 시작할 때 드롭다운 닫기
+              }, 10);
+            }}
+          >
+            <PencilIcon />
+          </button>
+        </div>
+        <div className='mt-1 text-caption-medium text-gray-60'>{dateLabel}</div>
+      </DropdownMenuLabel>
+
+      {/* 구분선 */}
+      <DropdownMenuSeparator className='my-2' />
+      {/* 드롭다운 목록 */}
+
+      {/* --- 저장 및 공유하기 --- 모달 */}
+      <DropdownMenuItem onClick={handleOpenShareModal}>
+        <Icon icon='tdesign:save' width='16' height='16' />
+        저장 및 공유하기
+      </DropdownMenuItem>
+
+      {/* --- 삭제하기 --- */}
+      <DropdownMenuItem onClick={handleDeleteCard} disabled={isDeleting}>
+        <DeleteIcon />
+        {isDeleting ? '삭제 중...' : '삭제하기'}
+      </DropdownMenuItem>
+
+      {/* --- 통계보기 --- */}
+      <DropdownMenuItem asChild>
+        <Link href={`${ROUTES.MYCARD}/${cardId}`}>
+          <Icon icon='tdesign:leaderboard' width='18' height='18' />
+          통계보기
+        </Link>
+      </DropdownMenuItem>
+
+      {/* --- 편집하기 : 에디터 --- */}
+      <DropdownMenuItem asChild>
+        <Link href={`${ROUTES.EDITOR}?cardId=${cardId}`}>
+          <Icon icon='tdesign:edit-2' width='18' height='18' />
+          편집하기
+        </Link>
+      </DropdownMenuItem>
+
+      {/* --- 공유화면 미리보기 --- */}
+      <DropdownMenuItem asChild>
+        <Link href={`/${slug}`}>
+          <PreviewIcon />
+          공유 화면 미리보기
+        </Link>
+      </DropdownMenuItem>
+    </>
+  );
+
   return (
     <div className='group relative z-50'>
       <DropdownMenu open={open} onOpenChange={setOpen}>
@@ -122,65 +194,7 @@ const CardEditDropdown = ({
           align='end'
           className='z-50 w-[206px]'
         >
-          {/* 드롭다운 라벨 */}
-          <DropdownMenuLabel className='flex flex-col'>
-            <div className='flex items-center justify-between text-label1-bold'>
-              {title}
-              <button
-                type='button'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  onEdit(); // 제목 편집모드 전환
-
-                  setTimeout(() => {
-                    setOpen(false); // 편집 시작할 때 드롭다운 닫기
-                  }, 10);
-                }}
-              >
-                <PencilIcon />
-              </button>
-            </div>
-            <div className='mt-1 text-caption-medium text-gray-60'>
-              {dateLabel}
-            </div>
-          </DropdownMenuLabel>
-
-          {/* 구분선 */}
-          <DropdownMenuSeparator className='my-2' />
-          {/* 드롭다운 목록 */}
-
-          {/* 공유하기 모달 */}
-          <DropdownMenuItem onClick={handleOpenShareModal}>
-            <Icon icon='tdesign:save' width='16' height='16' />
-            저장 및 공유하기
-          </DropdownMenuItem>
-
-          <DropdownMenuItem onClick={handleDeleteCard} disabled={isDeleting}>
-            <DeleteIcon />
-            {isDeleting ? '삭제 중...' : '삭제하기'}
-          </DropdownMenuItem>
-
-          <DropdownMenuItem asChild>
-            <Link href={`${ROUTES.MYCARD}/${cardId}`}>
-              <Icon icon='tdesign:leaderboard' width='18' height='18' />
-              통계보기
-            </Link>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem asChild>
-            <Link href={`${ROUTES.EDITOR}?cardId=${cardId}`}>
-              <Icon icon='tdesign:edit-2' width='18' height='18' />
-              편집하기
-            </Link>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem asChild>
-            <Link href={`/${slug}`}>
-              <PreviewIcon />
-              공유 화면 미리보기
-            </Link>
-          </DropdownMenuItem>
+          {/* 드롭다운 라벨부터, 공유 미리보기 까지 */}
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
