06172abf9b419a28b8629fde19c0ffde86a90308
woozizi <c980805@naver.com>
2025-04-28 15:19:23 +0900

    ♻️ refactor : 메인 인트로 섹션 반응형 #288

--- src/components/main/main-Intor-section.tsx
+++ src/components/main/main-Intor-section.tsx
@@ -10,34 +10,39 @@ const MainIntorSection = () => {
       className='relative h-[calc(100vh-64px)] bg-cover bg-center'
       style={{ backgroundImage: 'url(/main/main-bg.png)' }}
     >
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='flex flex-row items-center justify-center'>
-          <div className='flex flex-col space-y-[16px]'>
-            <Image
-              src='/main-logo-black.png'
-              alt='메인 대표 로고'
-              width={150}
-              height={64}
-            />
-            <p className='text-title-medium'>
+      <div className='absolute inset-0 flex items-center justify-center px-4'>
+        <div className='flex flex-col items-center justify-center gap-8 md:flex-row'>
+          {/*텍스트영역 */}
+          <div className='order-2 flex flex-col items-center space-y-[16px] text-center md:order-1 md:items-start md:text-left'>
+            <div className='relative h-[50px] w-[120px] md:h-[64px] md:w-[150px]'>
+              <Image
+                src='/main-logo-black.png'
+                alt='메인 대표 로고'
+                fill
+                style={{ objectFit: 'contain' }}
+                priority
+              />
+            </div>
+            <p className='text-body-medium md:text-title-medium'>
               누구나 쉽게 만들고, <br />
               간편하게 공유하는 스마트 명함
             </p>
-            <div className='flex flex-row gap-[12px]'>
+            <div className='flex w-full flex-row justify-center gap-[12px] md:w-auto'>
               <Link href={ROUTES.EDITOR}>
-                <button className='h-14 w-40 rounded-[46px] bg-primary-40 text-body-medium text-white'>
+                <button className='h-11 rounded-[46px] bg-primary-40 px-[22px] py-[6px] text-label2-medium text-white md:h-14 md:w-40 md:text-body-medium'>
                   바로 시작하기
                 </button>
               </Link>
               <Link href={ROUTES.TEMPLATES.BASE}>
-                <button className='h-14 w-40 rounded-[46px] bg-white text-body-medium'>
+                <button className='h-11 rounded-[46px] bg-white px-4 py-[6px] text-label2-medium md:h-14 md:w-40 md:text-body-medium'>
                   템플릿 보러가기
                 </button>
               </Link>
             </div>
           </div>
 
-          <div className='relative h-[650px] w-[510px]'>
+          {/*이미지영역 */}
+          <div className='relative order-1 h-[374px] w-[294px] md:order-2 md:h-[650px] md:w-[510px]'>
             <Image
               src='/main/main-card-img.png'
               alt='메인 대표 명함'
