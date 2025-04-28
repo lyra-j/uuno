bb3df32e1ee4f59b31b8daff58dfa473ff906103
lyra <mk.lyra.j@gmail.com>
2025-04-29 04:06:52 +0900

    🎉 add : 명함 제목 옵티미스틱 업데이트 #337

---
+++ src/hooks/mutations/use-update-card-title.ts
@@ -0,0 +1,63 @@
+import { patchCardTitle } from '@/apis/dashboard.client.api';
+import { QUERY_KEY } from '@/constants/query-key';
+import { authStore } from '@/store/auth.store';
+import { Cards } from '@/types/supabase.type';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+interface Payload {
+  cardId: string;
+  newTitle: string;
+}
+
+export const useUpdateCardTitle = (sortKey: string) => {
+  const queryClient = useQueryClient();
+
+  const userId = authStore((state) => state.userId);
+
+  return useMutation({
+    mutationFn: ({ cardId, newTitle }: Payload) =>
+      patchCardTitle(cardId, newTitle),
+    onMutate: async ({ cardId, newTitle }) => {
+      // 쿼리 취소
+      await queryClient.cancelQueries({
+        queryKey: [QUERY_KEY.SORT_CARD, userId, sortKey],
+      });
+
+      // 스냅샷
+      const prevSorted = queryClient.getQueryData<Cards[]>([
+        QUERY_KEY.SORT_CARD,
+        userId,
+        sortKey,
+      ]);
+
+      // 옵티미스틱
+      const now = new Date().toISOString();
+      queryClient.setQueryData<Cards[]>(
+        [QUERY_KEY.SORT_CARD, userId, sortKey],
+        (list) =>
+          list?.map((card) =>
+            card.id === cardId
+              ? { ...card, title: newTitle, updated_at: now }
+              : card
+          ) ?? []
+      );
+      // 롤백 컨텍스트
+      return prevSorted;
+    },
+    onError: (_err, _vars, context: any) => {
+      // 롤백
+      if (context?.prevSorted) {
+        queryClient.setQueryData(
+          [QUERY_KEY.SORT_CARD, userId, sortKey],
+          context.prevSorted
+        );
+      }
+    },
+    onSettled: () => {
+      // 서버 동기화
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.SORT_CARD, userId, sortKey],
+      });
+    },
+  });
+};
