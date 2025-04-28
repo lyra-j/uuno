6c6987ff5951abd656bb02ca201219d041f8f8eb
노수민 <nosumin29@gmail.com>
2025-04-29 05:57:55 +0900

    💄 style : 내 명함 상세 왼쪽 사이즈 컬럼 제공되지 않는 유틸 해결 #339

--- src/app/card/[...id]/page.tsx
+++ src/app/card/[...id]/page.tsx
@@ -87,7 +87,7 @@ const CardPage = async ({ params }: CardDetailProps) => {
         <div className='max-w-5xl md:mx-auto'>
           <div className='flex flex-col md:max-h-[calc(100vh-150px)] md:flex-row'>
             {/* 왼쪽 컬럼 */}
-            <div className='overflow-initial flex w-full flex-col border-r border-gray-5 px-[28px] py-3 text-body-regular shadow-[0px_3px_18px_0px_rgba(0,0,0,0.04)] md:w-[318px] md:overflow-auto'>
+            <div className='flex w-full flex-col overflow-visible border-r border-gray-5 px-[28px] py-3 text-body-regular shadow-[0px_3px_18px_0px_rgba(0,0,0,0.04)] md:w-[318px] md:overflow-auto'>
               <LeftNavSection cardTitle={cardData.title} />
             </div>
 
