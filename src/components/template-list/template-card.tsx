20f6dc74f969ac24953417b7cb6a5d08d07adef5
lyra <mk.lyra.j@gmail.com>
2025-04-28 15:35:16 +0900

    📄 docs : 주석 삭제 #331

--- src/components/template-list/template-card.tsx
+++ src/components/template-list/template-card.tsx
@@ -20,7 +20,6 @@ const TemplateCard = ({ template, onPreview }: Props) => {
 
   return (
     <li className='group relative aspect-[278/274] min-w-[278px] rounded'>
-      {/* TODO: 현재 썸네일 null 허용이지만 템플릿들이 올라갈때는 null 비허용으로 바꿀 예정입니다 */}
       <Image
         src={template.thumbnail ?? '/placeholder.png'}
         alt={template.name}
