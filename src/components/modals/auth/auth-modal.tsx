a954d7dd6d3690c4b899e3cc1c253b0e85813a03
K-jisu <wltn7star@naver.com>
2025-04-30 00:33:22 +0900

    🎉 add : 이미지 및 아이콘 컴포넌트 추가, 모달 상태에 따른 텍스트 수정, 반응형 #349

--- src/components/modals/auth/auth-modal.tsx
+++ src/components/modals/auth/auth-modal.tsx
@@ -3,6 +3,8 @@ import { modalStore } from '@/store/modal.store';
 import AuthLogin from './auth-login';
 import AuthSignup from './auth-signup';
 import AuthSignupEmail from './auth-signup-email';
+import Image from 'next/image';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const AuthModal = () => {
   const modalState = modalStore((state) => state.modalState);
@@ -16,21 +18,50 @@ const AuthModal = () => {
 
   return (
     <div
-      className='fixed inset-0 z-50 flex items-center justify-center bg-black/50'
+      className='fixed inset-0 z-50 flex justify-center bg-white sm:items-center sm:bg-black/50'
       onClick={() => {
+        if (window.innerWidth < 640) {
+          return;
+        }
         setOpenModal(false);
         setModalState('signup');
       }}
     >
       <div
-        className='flex w-[368px] flex-col items-center gap-[26px] rounded-xl bg-white p-6 px-[30px] py-10'
+        className='mt-[100px] flex w-[368px] flex-col items-center gap-[26px] rounded-xl bg-white p-6 px-[30px] py-10 sm:relative sm:mt-0'
         onClick={(e) => e.stopPropagation()}
       >
+        <Image
+          src={'/icons/close.svg'}
+          width={16}
+          height={16}
+          alt='닫기 버튼'
+          className='absolute right-[30px] top-10 cursor-pointer sm:right-5 sm:top-5'
+          onClick={() => {
+            setOpenModal(false);
+            setModalState('signup');
+          }}
+        />
+        {modalState === 'signup-email' && (
+          <Icon
+            icon='ci:chevron-left'
+            width='24'
+            height='24'
+            className='absolute left-[30px] top-10 cursor-pointer'
+            onClick={() => setModalState('signup')}
+          />
+        )}
         <div className='flex flex-col items-center gap-[46px] self-stretch'>
-          <div className='flex flex-col items-start gap-[26px] self-stretch'>
+          <div
+            className={`flex flex-col items-start gap-[${modalState === 'login' ? '60px' : modalState === 'signup-email' ? '26px' : '100px'}] self-stretch sm:gap-[26px]`}
+          >
             <div className='flex items-center justify-center gap-[17px] self-stretch'>
               <h2 className='text-heading-semi text-black'>
-                {modalState === 'login' ? 'Uuno 로그인' : 'Uuno 회원가입'}
+                {modalState === 'login'
+                  ? 'Uuno 로그인'
+                  : modalState === 'signup-email'
+                    ? '이메일로 회원가입'
+                    : 'Uuno 회원가입'}
               </h2>
             </div>
             {modalState === 'signup-email' && <AuthSignupEmail />}
