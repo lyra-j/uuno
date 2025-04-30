5b6b9d674e3d400d62b65ba5bf3d6bdbf50c57f9
K-jisu <wltn7star@naver.com>
2025-04-30 20:52:42 +0900

    ✨ update : 계정설정 페이지 구현 #335

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,18 +1,36 @@
-import { CommonButton } from '@/components/common/common-button';
-import { CommonInput } from '@/components/common/common-input';
-import { Icon } from '@iconify/react/dist/iconify.js';
+import { getUserDataServer } from '@/apis/user-server.api';
+import AccountSocialLogin from '@/components/dashboard/account/account-social-login';
+import AccountUserNameInput from '@/components/dashboard/account/account-user-name-input';
 
-import React from 'react';
+const AccountPage = async () => {
+  const { user } = await getUserDataServer();
+  if (!user) return;
 
-const AccountPage = () => {
+  const socialLogin = user.app_metadata.provider;
+
+  const fullName = user.user_metadata.full_name;
+
   return (
     <article className='rounded-xl bg-white px-12 py-8'>
       {/* 기본 정보 섹션 */}
-      <section className='flex h-[calc(100vh-16rem)] w-full items-center'>
-        <h3 className='flex w-full items-center justify-center pb-2 text-center text-body-medium text-black'>
-          계정 설정 페이지는 <br />
-          준비 중입니다.
-        </h3>
+      <section className='flex h-[calc(100vh-16rem)] w-full flex-col items-center'>
+        {/* 기본 정보 */}
+        <div className='flex w-full flex-col'>
+          <p className='text-label1-semi text-black'>기본 정보</p>
+
+          <div className='mt-2 w-full border border-b-gray-5' />
+
+          <AccountUserNameInput userName={fullName} />
+
+          <div className='mt-7 flex w-full flex-col gap-2'>
+            <p className='text-label2-bold text-black'>아이디</p>
+            <div className='flex h-11 w-full items-center gap-[10px] rounded border border-gray-10 px-5 py-[10px]'>
+              {user.email}
+            </div>
+          </div>
+        </div>
+
+        <AccountSocialLogin socialLogin={socialLogin} />
       </section>
     </article>
   );
