fac09b23f9367dd6624b0d6c92c1624b8241c582
KJSoo95 <wltn7star@naver.com>
2025-05-09 16:01:10 +0900

    ✨ update : 개선된 닉네임 업데이트 로직 및 오류 처리 추가#335

--- src/components/dashboard/account/account-user-name-input.tsx
+++ src/components/dashboard/account/account-user-name-input.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import { userNameUpdate } from '@/apis/user-name-update.api';
+import {  VALIDATE } from '@/constants/auth.messages.constant';
+import { toastSuccess } from '@/lib/toast-util';
 import { authStore } from '@/store/auth.store';
+import { nickNameSchema } from '@/utils/auth/auth-validate-schema';
+import { duplicateNickNameValidation } from '@/utils/auth/duplicate-validation';
 import { useEffect, useState } from 'react';
 
 interface AccountUserNameInputProps {
@@ -10,17 +14,33 @@ interface AccountUserNameInputProps {
 
 const AccountUserNameInput = ({ userName }: AccountUserNameInputProps) => {
   const [userNickName, setUserNickName] = useState(userName);
+  const [error, setError] = useState('');
   const setUserName = authStore((state) => state.setUserName);
 
   const handleNickNameUpdate = async () => {
+    const result = nickNameSchema.safeParse(userNickName);
+    if (!result.success) {
+      setError(result.error.issues[0].message);
+      return;
+    }
+
+    const status = await duplicateNickNameValidation(userNickName);
+
+    if (status) {
+      setError(VALIDATE.DUPLICATED_NICKNAME);
+      return;
+    }
+
     await userNameUpdate(userNickName);
     setUserName(userNickName);
+    toastSuccess('닉네임이 변경되었습니다!');
+    setError('');
   };
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       handleNickNameUpdate();
-    }, 2000);
+    }, 800);
 
     return () => clearTimeout(timeout);
   }, [userNickName]);
@@ -34,6 +54,7 @@ const AccountUserNameInput = ({ userName }: AccountUserNameInputProps) => {
         onChange={(e) => setUserNickName(e.target.value)}
         className='flex h-11 w-full items-center gap-[10px] rounded border border-gray-10 px-5 py-[10px]'
       />
+      <p className='text-caption-medium text-error'>{error}</p>
     </div>
   );
 };
